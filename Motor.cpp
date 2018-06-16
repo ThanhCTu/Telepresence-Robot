ab0c85334943af82947c741498817db898be5942
Thanh Tu <thanh.tu01@aol.com>
2018-06-15 20:49:47 -0400

    Initialize workload

---
+++ Motor.cpp
@@ -0,0 +1,18 @@
+/*
+ * Motor.cpp
+ *
+ *  Created on: Jun 15, 2018
+ *      Author: Thanh Tu
+ */
+
+#include "Motor.h"
+
+Motor::Motor() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Motor::~Motor() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ Motor.h
@@ -0,0 +1,17 @@
+/*
+ * Motor.h
+ *
+ *  Created on: Jun 15, 2018
+ *      Author: Thanh Tu
+ */
+
+#ifndef MOTOR_H_
+#define MOTOR_H_
+
+class Motor {
+public:
+	Motor();
+	virtual ~Motor();
+};
+
+#endif /* MOTOR_H_ */
